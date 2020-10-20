490cb72d9a1073df1aa0824569305c1c4fd8b12c
tastr <Strelnikovatatyana@gmail.com>
2020-10-20 14:21:29 +0200

    feat: add Tobias's FD example

--- MATLAB/FD_ex.m
+++ MATLAB/FD_ex.m
@@ -9,9 +9,9 @@ clear all
 %% Define system and controller
 % Define system (from Lecture)
 s = zpk('s');
-sample_time = 1e-3;
+sample_time = 0.5*1e-1;
 sys = ss(0.036*(s+25.28)/(s^2*(s^2+0.0396*s+1)));
-N = 10;
+N = 431;
 
 %Define plant 
 sys = c2d(sys,sample_time);
@@ -32,7 +32,105 @@ cloop = lft(P, Kc);
 S = -cloop;
 T = 1 - S; 
 
+
+%% LQR
+[A, B, C, D] = ssdata(sys);
+[A,B,C,D, N] = get_non0D_system(A,B,C,D, N);
+N
+RP = 0.001; % Weights...
+QP = diag([10, 1, 10, 10]);
+RQ = 0.001;
+QQ = 10*eye(size(A, 1));
+P = idare(A, B, QP, RP);   
+Q = idare(A', C', QQ, RQ);
+
+% l-input m-output system
+l = length(B(1,:));
+m = length(C(:,1));
+
+%Calculate the matrices for separation principle based controller
+F = (B' * P * B + RP) \ (B' * P * A);
+J = ((C * Q * C' + RQ) \ (C * Q * A'))';
+M = 5;
+rng default;
+r_vec = kron(rand(M, 1), ones(N/M, 1));
+
+eig(A - B * F)
+eig(A - J*C)
+x0 = zeros(size(A, 1), 1);
+%x0 = randn(size(x0));
+
+% tracking with LQR
+u_sv = [];%input supervector
+y_sv = [];%output supervector
+
+% Calculate matrix for reference tracking
+TMP = (eye(length(A)) - (A - B*F));
+TMP = TMP\eye(length(A));
+M = (C - D*F)*TMP*B + D;
+M = M\eye(length(M));
+
+
+x_hat = x0;
+x = x0;
+for i = 1:N
+    r = r_vec(i);
+    u = -F*x_hat + M*r; 
+    y = C*x + D*u;
+    x_hat = (A - J*C)*x_hat + (B - J*D)*u + J*y;
+    
+    x = A*x + B*u;   
+    
+    u_sv = [u_sv; u];
+    y_sv = [y_sv; y];
+end
+
+% Plot
+close all
+hold on
+plot(0:N-1, y_sv);
+plot(0:N-1, r_vec);
+legend('output $y(t)$', 'reference signal', 'interpreter', 'latex');
+xlabel('time $t$', 'interpreter', 'latex');
+ylabel('output and reference');
+xlim([0, N-1]);
+hold off
+
+%% Inverse Algorithm
+u0 = u_sv;
+r = r_vec;
+R = 1;
+Q = 1;
+do_plot = 1;
+b = .1; 
+[G, d] = get_G(A-B*F, B, C - D*F, D, x0, N-1);
+close all
+[u_inf1, e_inf1, y_inf1, impr1, iteration_number1, error_history1] = IA(G,D, d, b,r, u0, do_plot, 1);
+xmax = iteration_number1;
+if(xmax>0)
+    xlim([0, xmax])
+end
+legend('$e_k$', 'interpreter', 'latex');
+
+% Plot results LQR vs IA 
+close all
+hold on
+plot(0:N-1, y_sv);
+plot(0:N-1, r, 'LineWidth', 1.8);
+plot(0:N-1, y_inf1{end}, '--');
+plot(0:N-1, y_inf1{1}, '--');
+plot(0:N-1, y_inf1{2}, '--');
+legend('LQR', 'Reference', 'IA');
+xlim([0, N-1])
+hold off
+
+
 %% Get supervector form
+% Ob D = 0 ist sollte hier doch immer egal sein?
+% Ich glaube auch er definiert T als (I + GK)^{-1}GK
+% wobei G und K die supervektor matrizen sind..
+% Das sollte aber wohl auch im direkten zusammenhang zum den supervekoren
+% matrizen von T stehen. (hoffentlich)
 
 Nmin = N; % The minimal possible time horizon over all systems
 [A, B, C, D] = ssdata(K);
@@ -89,7 +187,7 @@ while cont
     error_history = [error_history; norm(e_new)];
 
     %termination criteria 
-    if(norm(e - e_new)<1e-6)
+    if(norm(e - e_new)<1e-5)
         cont = 0;
     end
     
@@ -108,3 +206,7 @@ end
 disp(['e_inf = ', num2str(error_history(end))])
 
 plot(0:iteration_number-1, error_history);
+
+%%
+
+
